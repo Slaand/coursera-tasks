f424e0d9c6a44d2929ef99880add7bc2a5a3fb8e
Aleksandrs <owner@slaand.com>
2017-09-05 19:13:47 +0300

    First commit 2

--- percolation/src/Percolation.java
+++ percolation/src/Percolation.java
@@ -1,11 +1,9 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private boolean[][] status;
-    private int top;
+    private byte[][] grid;
+    private final int top = 0;
     private int bottom;
     private int size;
     private WeightedQuickUnionUF wqu;
@@ -15,31 +13,61 @@ public class Percolation {
         if (n < 1) throw new IllegalArgumentException("N isn't greater than 0");
 
         size = n;
-        top = 0; bottom = size*size;
-        status = new boolean[size][size];
-        wqu = new WeightedQuickUnionUF(size);
+        bottom = size*size+1;
+        grid = new byte[size][size];
+
+        wqu = new WeightedQuickUnionUF(bottom);
 
     }
 
     public void open(int row, int col) {
 
-        status[row][col] = true;
-        wqu.union(row, col);
+        grid[row-1][col-1] = 1;
+
+        if (row == 1) {
+            wqu.union(index(row, col), top);
+        }
+        if (row == size) {
+            wqu.union(index(row, col), bottom-1);
+        }
+
+        if (col > 1 && isOpen(row, col - 1)) {
+            wqu.union(index(row, col), index(row, col - 1));
+        }
+        if (col < size && isOpen(row, col + 1)) {
+            wqu.union(index(row, col), index(row, col + 1));
+        }
+        if (row > 1 && isOpen(row - 1, col)) {
+            wqu.union(index(row, col), index(row - 1, col));
+        }
+        if (row < size && isOpen(row + 1, col)) {
+            wqu.union(index(row, col), index(row + 1, col));
+        }
     }
 
     public boolean isOpen(int row, int col) {
-        return status[row][col];
+        if(grid[row-1][col-1] == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean isFull(int row, int col) {
-        if(0 < row && row <= size && 0 < col && col <= size) {
-            return wqu.connected(row-1, col-1);
+
+        if ((row <= 0 || row > size) && (col <= 0 || col > size)) {
+            throw new ArrayIndexOutOfBoundsException("row index out of bounds");
         } else {
-            throw new ArrayIndexOutOfBoundsException();
+            return wqu.connected(top, index(row, col));
         }
     }
 
     public boolean percolates() {
-        return wqu.connected(top, bottom);
+
+        return wqu.connected(top,bottom-1);
+    }
+
+    private int index(int row, int col) {
+        return size * (row-1) + col;
     }
 }
--- percolation/src/PercolationStats.java
+++ percolation/src/PercolationStats.java
@@ -1,33 +1,67 @@
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
+    private double[] dafuk;
+    private int trials;
+    private int n;
+    private Percolation perc;
+
     public PercolationStats(int n, int trials) {
 
+        this.n = n;
+        this.trials = trials;
+
+        if (n < 0 || trials < 1) {
+            throw new IllegalArgumentException("N or T < 0");
+        }
+
+        dafuk = new double[trials];
+        for(int i=0; i<trials; i++) {
+            perc = new Percolation(n);
+            while(!perc.percolates()) {
+                int pop = StdRandom.uniform(1,n+1);
+                int pup = StdRandom.uniform(1,n+1);
+                if(!perc.isOpen(pop,pup)) {
+                    perc.isOpen(pop,pup);
+                }
+            }
+        }
     }
 
     public double mean() {
-        return 0;
+        return StdStats.mean(dafuk);
     }
 
     public double stddev() {
-        return 0;
+        return StdStats.stddev(dafuk);
     }
 
     public double confidenceLo() {
-        return 0;
+        return mean() - (1.96*stddev()/Math.sqrt(trials));
     }
 
     public double confidenceHi() {
-        return 0;
+        return mean() + (1.96*stddev()/Math.sqrt(trials));
     }
 
     public static void main(String[] args) {
 
-        int N = Integer.parseInt(args[0]);
-        int M = Integer.parseInt(args[1]);
+        if (args.length < 2) {
+            System.out.println("Not enough args");
+        }
 
+        final int N = Integer.parseInt(args[0]);
+        final int M = Integer.parseInt(args[1]);
 
+        PercolationStats percstat = new PercolationStats(N, M);
+
+        System.out.println("mean = " +percstat.mean());
+        System.out.println("stddev = " +percstat.stddev());
+        System.out.println("conflo = " +percstat.confidenceLo());
+        System.out.println("confhi = " +percstat.confidenceHi());
 
     }
 }
